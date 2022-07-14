17f8745e0d175d724326f11bec2b1d35459a7c8b
Thomas Pendleton <thomas6576@yahoo.com>
2022-07-13 21:55:57 -0700

    adding to home page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/profile.jpg'
+import ME from '../../assets/profile.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
-    return (
-       <header>
-        <div className="container header_container">
-          <h5>Hello I'm</h5>
-          <h1>Thomas Pendleton</h1>
-          <h5 className="text-light">Fullstack Developer</h5>
-          <CTA />
-          <div className="me">
-            <img src={ME} alt="me" />
-          </div>
-          <a href="#contact" className='scroll_down'>Scroll Down</a>
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Thomas Pendleton</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials /> 
+        <div className="me">
+          <img src={ME} alt="me" />
         </div>
-       </header>
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
   )
 }
 
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithubSquare } from 'react-icons/fa'
+import { FaDribbbleSquare } from 'react-icons/fa'
 
 const HeaderSocials = () => {
   return (
     <div className="header_socials">
-        <a href="https://linkedin.com" target='_blank'></a>
-        <a href="https://github.com" target='_blank'></a>
-        <a href="https://dribbble.com" target='_blank'></a>
+      <a href="https://linkedin.com" target="_blank">
+        <FaLinkedin />
+      </a>
+      <a href="https://github.com" target="_blank">
+        <FaGithubSquare />
+      </a>
+      <a href="https://dribbble.com" target="_blank">
+        <FaDribbbleSquare />
+      </a>
     </div>
   )
 }
