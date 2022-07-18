467ee8b66ed314e934d13891030f33d0788fc07b
Thomas Pendleton <thomas6576@yahoo.com>
2022-07-18 12:21:52 -0700

    adding to services component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,24 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id="portfolio">
+      <h5>My recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio_container">
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+
+          </div>
+          <h3>Portfolio itme title</h3>
+          <a href="https://github.com/thomaspendleton" className="btn">
+            Github
+          </a>
+          <a href="https://github.com/thomaspendleton" className="btn btn-primary">
+            Live demo
+          </a>
+        </article>
+      </div>
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,103 @@
 import React from 'react'
 import './services.css'
+import { GiCheckMark } from 'react-icons/gi'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container services_container">
+        <article className="service">
+          <div className="service_head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <GiCheckMark className="service_list-icon" />
+              <p>Lorem adipisicing elit. Odit aperiam mollitia.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service_list-icon" />
+              <p>Lorem adipisicing elit. Odit aperiam mollitia.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service_list-icon" />
+              <p>Lorem adipisicing elit. Odit aperiam mollitia.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service_list-icon" />
+              <p>Lorem adipisicing elit. Odit aperiam mollitia.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service_list-icon" />
+              <p>Lorem adipisicing elit. Odit aperiam mollitia.</p>
+            </li>
+          </ul>{' '}
+        </article>
+        {/* End of UX/UI */}
+
+        <article className="service">
+          <div className="service_head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="service_list">
+            <li>
+              <GiCheckMark className="service_list-icon" />
+              <p>Lorem adipisicing elit. Odit aperiam mollitia.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service_list-icon" />
+              <p>Lorem adipisicing elit. Odit aperiam mollitia.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service_list-icon" />
+              <p>Lorem adipisicing elit. Odit aperiam mollitia.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service_list-icon" />
+              <p>Lorem adipisicing elit. Odit aperiam mollitia.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service_list-icon" />
+              <p>Lorem adipisicing elit. Odit aperiam mollitia.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service_list-icon" />
+              <p>Lorem adipisicing elit. Odit aperiam mollitia.</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of  */}
+        <article className="service">
+          <div className="service_head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className="service_list">
+            <li>
+              <GiCheckMark className="service_list-icon" />
+              <p>Lorem adipisicing elit. Odit aperiam mollitia.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service_list-icon" />
+              <p>Lorem adipisicing elit. Odit aperiam mollitia.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service_list-icon" />
+              <p>Lorem adipisicing elit. Odit aperiam mollitia.</p>
+            </li>
+            <li>
+              <GiCheckMark className="service_list-icon" />
+              <p>Lorem adipisicing elit. Odit aperiam mollitia.</p>
+            </li>
+           
+          </ul>
+        </article>
+        {/* End of content creation */}
+      </div>
+    </section>
   )
 }
 
