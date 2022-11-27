66f1f25041db325ae1170ede17e871f89575e95e
Thomas Pendleton <thomas6576@yahoo.com>
2022-11-26 21:47:52 -0800

    add alternate styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       <div className="container header_container">
         <h5>Hello I'm</h5>
         <h1>Thomas Pendleton</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="text-light">Frontend Developer</h5>
         <CTA />
         <HeaderSocials />
         <div className="me">
