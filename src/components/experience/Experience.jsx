063a45a835ad68b15dab048909723f262037220b
Thomas Pendleton <thomas6576@yahoo.com>
2022-12-21 16:51:04 -0800

    update project data and experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -87,7 +87,7 @@ const Experience = () => {
             <article className="experience_details">
               <IoMdCheckmarkCircleOutline className="experience_details-icon" />
               <div>
-                <h4>GraphQL</h4>
+                <h4>Postgres</h4>
                 <small className="text-light">Basic</small>
               </div>
             </article>
--- src/data.js
+++ src/data.js
@@ -1,40 +1,41 @@
 import IMG1 from './assets/reactMathGame.png'
 import IMG2 from './assets/footballScreenShot.png'
 import IMG3 from './assets/proj3.png'
-
+import IMG4 from "./assets/proj4.png"
 
 export const data = [
   {
     id: 1,
     image: IMG1,
-    title: 'Math Game',
-    github: 'https://github.com/thomasPendleton/math_game_react',
-    demo: 'https://multiply-math-game.netlify.app/',
-    tech: 'React, context, styled components, local storage'
+    title: "Math Game",
+    github: "https://github.com/thomasPendleton/math_game_react",
+    demo: "https://multiply-math-game.netlify.app/",
+    tech: "React, context, styled components, local storage, postgres database",
   },
   {
     id: 2,
     image: IMG2,
-    title: 'Football Data App',
-    github: 'https://github.com/thomasPendleton/react_football',
-    demo: 'https://nfl-schedule.netlify.app/',
-    tech: 'React, Redux toolkit, SportsData.io API, styled components'
-  },
-  {
-    id: 3,
-    image: IMG3,
-    title: 'Cocktail db search',
-    github: 'https://github.com/thomasPendleton/cocktails',
-    demo: 'https://lustrous-palmier-aa799e.netlify.app',
-    tech: 'React, context, styled components'
+    title: "Football Data App",
+    github: "https://github.com/thomasPendleton/react_football",
+    demo: "https://nfl-schedule.netlify.app/",
+    tech: "React, Redux toolkit, SportsData.io API, styled components",
   },
   // {
-  //   id: 4,
-  //   image: IMG4,
-  //   title: 'Math Game',
-  //   github: 'https://github.com/thomasPendleton/mathGame',
-  //   demo: 'https://thomaspendleton.github.io/mathGame/',
+  //   id: 3,
+  //   image: IMG3,
+  //   title: 'Cocktail db search',
+  //   github: 'https://github.com/thomasPendleton/cocktails',
+  //   demo: 'https://lustrous-palmier-aa799e.netlify.app',
+  //   tech: 'React, context, styled components'
   // },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Business Website",
+    github: "https://github.com/thomasPendleton/abaBusiness",
+    demo: "https://calm-creponne-aab12e.netlify.app/",
+    tech: "React, context, styled components",
+  },
   // {
   //   id: 5,
   //   // image: IMG5,
